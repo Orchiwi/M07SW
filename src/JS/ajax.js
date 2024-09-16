5d374f01bd855787c588f26645daf4e362ac4b0c
Orchiwi <orchiwideveloper@outlook.com>
2024-09-16 09:04:20 +0200

    onclick donneesdrone

--- src/JS/ajax.js
+++ src/JS/ajax.js
@@ -1,26 +1,29 @@
-window.addEventListener("load", loadcorrectpage);
+// window.addEventListener("load", loadcorrectpage);
+// document.getElementById("donneesdrone").addEventListener("click",recupererDonneesDrones);
 document.getElementById("nav_suivi").addEventListener("click", suivi);
 
 
 
-function loadcorrectpage()
-{
-    let page = getCookie("page");
-    console.debug("cookie page : " + page);
-    if(page == "suivi")
-    {
-        suivi();
-    }
-    // else if(page == "message")
-    // {
-    //     message();
-    // }
-    // else
-    // {
-    //     presentation();
-    // }
 
-}
+
+// function loadcorrectpage()
+// {
+//     let page = getCookie("page");
+//     console.debug("cookie page : " + page);
+//     if(page == "suivi")
+//     {
+//         suivi();
+//     }
+//     else if(page == "message")
+//     {
+//         message();
+//     }
+//     else
+//     {
+//         presentation();
+//     }
+
+// }
 
 function suivi() {
     console.debug("Suivi ! ");
@@ -28,7 +31,7 @@ function suivi() {
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             var reponse = xhttp.responseText;
-            console.debug(reponse);
+            // console.debug(reponse);
             var pagehtml = document.getElementById( 'section' );
             pagehtml.innerHTML = reponse;
 
@@ -38,9 +41,48 @@ function suivi() {
     xhttp.open("GET", "mainDrone.html", true);
     xhttp.send();
     setCookie("page","suivi",1); 
-    recupererStatistique()
+   recupererStatistique()
+   
 
 
+}
+
+function recupererDonneesDrones(){
+
+  console.debug("Donnees Drones ! ");
+  // var xhttp = new XMLHttpRequest();
+  // xhttp.onreadystatechange = function() {
+  //     if (this.readyState == 4 && this.status == 200) {
+
+
+
+
+
+
+//   let reponseAPI=JSON.parse(this.responseText);
+// var table="<div ><table class='tableau_statistique '><tr class='centrer'><th>Numérodrône</th><th>Marque</th><th>Modèle</th><th>Référence</th><th>Dateachat</th><th>Action</th></tr>";
+//  for(let i=0;i<reponseAPI.valeur.length;i++){
+//  table+="<tr class='centrer'>";
+//  let donneesVol=reponseAPI.valeur[i];
+//  table+="<td>"+donneesVol.idvol+"</td>";
+//  table+="<td>"+donneesVol.dateVol+"</td>";
+//  table+="<td>"+donneesVol.iddrone+"</td>";
+//  table+="<td>"+donneesVol.nom+"</td>";
+//  table+="</tr>";
+//  }
+//  table+="</table></div>";
+// document.getElementById("section").innerHTML=table;
+
+
+
+
+
+
+// }
+// };
+// xhttp.open("GET", "mainDrone.html", true);
+// xhttp.send();
+// setCookie("page","suivi",1); 
 }
 
 function recupererStatistique(){
